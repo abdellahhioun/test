935ca2a0a48cd59765c8afbad17de3134961da31
alkadabna <abd-ellah.hioun@laplateforme.io>
2024-09-20 16:18:58 +0200

    Update get_friends_list.php

--- get_friends_list.php
+++ get_friends_list.php
@@ -1,11 +1,10 @@
 <?php
 include 'db_conn.php';
 
-// Assuming the user ID is stored in the session
 session_start();
 $userId = $_SESSION['user_id'];
 
-$stmt = $conn->prepare("SELECT u.id, u.name FROM friends f JOIN users u ON f.friend_id = u.id WHERE f.user_id = ?");
+$stmt = $conn->prepare("SELECT u.id, u.username, COALESCE(u.pfp, 'assets/default.png') AS pfp FROM friends f JOIN users u ON f.friend_id = u.id WHERE f.user_id = ?");
 $stmt->bind_param("i", $userId);
 $stmt->execute();
 $result = $stmt->get_result();
