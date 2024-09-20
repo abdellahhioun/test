7e606621a3daef30330c7cbf6806365e4882e755
alkadabna <abd-ellah.hioun@laplateforme.io>
2024-09-20 16:18:56 +0200

    Update friends_list.php

--- friends_list.php
+++ friends_list.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Friends List</title>
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
     <style>
         .pfp { width: 30px; height: 30px; border-radius: 50%; }
     </style>
@@ -39,22 +40,24 @@ $stmt->close();
 $conn->close();
 ?>
 
-<h1>Welcome, <?php echo htmlspecialchars($username); ?>!</h1>
-<img src="<?php echo htmlspecialchars($userPfp); ?>" alt="Profile Picture" class="pfp">
-<a href="profile.php?username=<?php echo htmlspecialchars($username); ?>">My Profile</a>
+<div class="container mt-4">
+    <h1>Welcome, <?php echo htmlspecialchars($username); ?>!</h1>
+    <img src="<?php echo htmlspecialchars($userPfp); ?>" alt="Profile Picture" class="pfp rounded-circle">
+    <a href="profile.php?username=<?php echo htmlspecialchars($username); ?>" class="btn btn-info ml-2">My Profile</a>
 
-<h2>Friends List</h2>
-<ul>
-<?php foreach ($friends as $friend): ?>
-    <li>
-        <img src="<?php echo htmlspecialchars($friend['pfp']); ?>" alt="Profile Picture" class="pfp">
-        <a href="profile.php?username=<?php echo htmlspecialchars($friend['username']); ?>">
-            <?php echo htmlspecialchars($friend['username']); ?>
-        </a>
-        <button onclick="startChat(<?php echo $friend['id']; ?>)">Chat</button>
-    </li>
-<?php endforeach; ?>
-</ul>
+    <h2 class="mt-4">Friends List</h2>
+    <ul class="list-group">
+    <?php foreach ($friends as $friend): ?>
+        <li class="list-group-item d-flex align-items-center">
+            <img src="<?php echo htmlspecialchars($friend['pfp']); ?>" alt="Profile Picture" class="pfp mr-2">
+            <a href="profile.php?username=<?php echo htmlspecialchars($friend['username']); ?>" class="mr-auto">
+                <?php echo htmlspecialchars($friend['username']); ?>
+            </a>
+            <button onclick="startChat(<?php echo $friend['id']; ?>)" class="btn btn-primary">Chat</button>
+        </li>
+    <?php endforeach; ?>
+    </ul>
+</div>
 
 <script>
 function startChat(friendId) {
@@ -62,5 +65,9 @@ function startChat(friendId) {
     window.location.href = `private_chat.php?friend_id=${friendId}`;
 }
 </script>
+
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </body>
 </html>
